89a50203d6e3535617a5ac4e168fd6faabfb681e
camerunyan <31229356+camerunyan@users.noreply.github.com>
2017-08-30 20:21:17 -0600

    Add files via upload

---
+++ Classes.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Classes : MonoBehaviour {
+
+	public class One {
+		public int money = 1000000;
+
+		public float fuel = 0.01f;
+
+		private string name = "Jessie James";
+
+	}
+
+	public class Two {
+
+		public int maxHealth = 100;
+
+		public int currentHealth = 100;
+
+		public int bullets = 36;
+	}
+
+	public class Three {
+
+
+	}
+}
